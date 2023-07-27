1051d4acc74828f7d27a46fce002270ca774c9aa
terremotoparatodos <105179879+terremotoparatodos@users.noreply.github.com>
2023-07-27 18:52:43 -0300

    Update config.js

--- config/config.js
+++ config/config.js
@@ -19,9 +19,9 @@ Config.whitelist = [
 // address bar is `Config.routes.client`.
 Config.defaultserver = {
 	id: 'showdown',
-	host: 'terremoto.live.psim.us',
+	host: 'terremoto.live',
 	port: 443,
-	httpport: 8081,
+	httpport: 8000,
 	altport: 80,
 	registered: true
 };
